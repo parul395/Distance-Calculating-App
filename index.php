c016daa7e096f6ad1cca741d98fc4d5dad4c9558
parul395 <75976543+parul395@users.noreply.github.com>
2021-01-28 12:06:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -63,9 +63,9 @@
       
       
       </div>
-      <!--AIzaSyBLd0Hl7PVa0PnyZVIlLrAUipqZ6npRSYA-->
+      
    
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLd0Hl7PVa0PnyZVIlLrAUipqZ6npRSYA&libraries=places"></script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=getyourownapikey=places"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     
     <script src="js/bootstrap.min.js"></script>
